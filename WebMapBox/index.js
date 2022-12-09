29eb2c11b107cb4d3b0c7c9b1875651ff5d45ee8
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-12-09 13:20:14 +0100

    Pagina Web

---
+++ WebMapBox/index.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlZGVsZW9uZXoiLCJhIjoiY2xhdGQxZDd6MmJmMzN4bGJieDMyZmJleCJ9.bIFVpM-jk-poGfoeuFgnZg'
+
+let map = new mapboxgl.Map({
+
+    container:'map',
+    style: 'mapbox://styles/aledeleonez/clbggmqkr003214t8x45ty2sx',
+    center:[-3.7025600,40.4165000],
+    zoom: 3
+
+})
