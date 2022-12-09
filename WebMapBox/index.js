1a6b16b700242db524f52d910591300f350b7e91
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-12-09 19:51:11 +0100

    mapas en la web

--- WebMapBox/index.js
+++ WebMapBox/index.js
@@ -1,10 +1,24 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlZGVsZW9uZXoiLCJhIjoiY2xhdGQxZDd6MmJmMzN4bGJieDMyZmJleCJ9.bIFVpM-jk-poGfoeuFgnZg'
 
-let map = new mapboxgl.Map({
+function provincias(){
+    let map = new mapboxgl.Map({
 
-    container:'map',
-    style: 'mapbox://styles/aledeleonez/clbggmqkr003214t8x45ty2sx',
-    center:[-3.7025600,40.4165000],
-    zoom: 3
+        container:'map',
+        style: 'mapbox://styles/aledeleonez/clbguep5f005i14nnhi5ab6j9',
+        center:[-3.7025600,40.4165000],
+        zoom: 3
+    
+    })
+}
 
-})
+function ciudades(){
+    let map = new mapboxgl.Map({
+
+        container:'map',
+        style: 'mapbox://styles/aledeleonez/clbguv4fq006c14o0p9w9itdo',
+        center:[-3.7025600,40.4165000],
+        zoom: 6
+    
+    })
+}
+
