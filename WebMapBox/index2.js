8a430db7af61780e390efefec47a9fe1ebcfcea2
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-12-11 17:13:14 +0100

    Implementaciones en web

---
+++ WebMapBox/index2.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlZGVsZW9uZXoiLCJhIjoiY2xhdGQxZDd6MmJmMzN4bGJieDMyZmJleCJ9.bIFVpM-jk-poGfoeuFgnZg'
+
+let map = new mapboxgl.Map({
+    container:'ciudades',
+    style: 'mapbox://styles/aledeleonez/clbguv4fq006c14o0p9w9itdo',
+    center:[-3.7025600,40.4165000],
+    zoom: 6
+
+})
