d06f88f2ddd9a04f1066190b05b7182205116ed3
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-12-12 18:37:47 +0100

    cambios js

--- WebMapBox/prov.js
+++ WebMapBox/prov.js
@@ -8,21 +8,9 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlZGVsZW9uZXoiLCJhIjoiY2xhdGQxZDd6MmJmMzN4b
     
     })
 
+
     
-
-    function provincias(){
-        return prov
-    }
-
-    function ciudades(){
-        let ciu = new mapboxgl.Map({
-            container:'ciudades',
-            style: 'mapbox://styles/aledeleonez/clbguv4fq006c14o0p9w9itdo',
-            center:[-3.7025600,40.4165000],
-            zoom: 6
-        
-        })
-    }
+    
 
 
    
